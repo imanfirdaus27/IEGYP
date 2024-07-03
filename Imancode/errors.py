d99c162815fdf88a1e45f556753b1b2f024e0201
imanfirdaus27 <firdausrostan@gmail.com>
2024-07-03 22:28:04 +0800

    put in my github

--- Imancode/errors.py
+++ Imancode/errors.py
@@ -4,7 +4,7 @@ x = 10
 # print("x is even")
 
 # logical error
-# if (x % 2 ==0):
+# if (x % 2 == 0):
 #     print(f"Given Number is {x}")
 # print("Even Number")
 
@@ -14,13 +14,25 @@ x = 10
 
 try:
     principle = int(input("Principle: "))
-    period = int(input("Period: "))
-    rate = int(input("Rate: "))
-    interest = (principle * period * rate) / 100
-    print("Interest Amount:", interest)
-except:
+
+except ValueError:
     # when that error occur what we must do
     print("Principle amount must be an Integer")
+
+except Exception as e:
+    print("Something went wrong:", e)
+else:
+    # if there is no error then this block will execute
+    print("All is well")
+finally:
+    # The code inside this block will always executed
+    print("Thank You")
+
+
+period = int(input("Period: "))
+rate = int(input("Rate: "))
+interest = (principle * period * rate) / 100
+print("Interest Amount:", interest)
 
 
 
